0bee48fe6b94e198ddc5c60b2e5d0aa059b75bb2
Pascalbuehlersluz <168638080+Pascalbuehlersluz@users.noreply.github.com>
2024-11-22 15:19:42 +0100

    Update main.go

--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -3,19 +3,36 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
 func main() {
-	var eyes = rand.Intn(5) + 1
+	rand.Seed(time.Now().UnixNano()) 
+	var eyes = rand.Intn(6) + 1      // Zahl ZWISCHEN 1 und6
 	var when = time.Now()
 
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice shows", eyes, "eyes")
+	
+	fmt.Fprintln(os.Stdout, "The dice shows", eyes, "eyes")
+	fmt.Fprintln(os.Stderr, "The dice was rolled at", when)
 
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice was rolled at", when)
+	
+	eyesFile, err := os.Create("eyes.txt")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating eyes.txt:", err)
+		return
+	}
+	defer eyesFile.Close()
 
-	// TODO: how to write the output into eyes.txt and dice.log?
-	// go run ex3/main.go TODO
+	fmt.Fprintln(eyesFile, eyes) // Only writing the number of eyes
+
+	
+	logFile, err := os.Create("dice.log")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating dice.log:", err)
+		return
+	}
+	defer logFile.Close()
+
+	fmt.Fprintf(logFile, "Dice roll: %d eyes, rolled at: %s\n", eyes, when)
 }
