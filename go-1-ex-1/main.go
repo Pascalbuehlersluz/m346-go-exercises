00b65eefe030f1da2346f1948100957deae9dd38
Pascalbuehlersluz <168638080+Pascalbuehlersluz@users.noreply.github.com>
2024-11-22 15:08:46 +0100

    Update main.go

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,10 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: Declare and initialize the variables being used in the output!
-	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
-	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
-	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
-	fmt.Printf("Grösse (in Metern): %.2f\n", heightInMeters)
-	fmt.Printf("Sternzeichen: %c\n", zodiacSign)
+    firstName := "Max"          
+    lastName := "Mustermann"    
+    dayOfBirth := 15            
+    monthOfBirth := 8           
+    yearOfBirth := 1995         
+    numberOfSiblings := 2       
+    heightInMeters := 1.75      
+    zodiacSign := '\u2648'      
+
+   
+    fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
+    fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
+    fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
+    fmt.Printf("Grösse (in Metern): %.2f\n", heightInMeters)
+    fmt.Printf("Sternzeichen: %c\n", zodiacSign)
 }
