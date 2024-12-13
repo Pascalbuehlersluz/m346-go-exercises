22a29087ee8b516661971d04cd0b0558fc864960
Pascal Buehler <Pascal_Buehler@sluz.ch>
2024-12-13 15:29:28 +0100

    noch nix fertig

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -7,4 +7,15 @@ const (
 
 func main() {
 	// TODO: Implement FizzBuzz using a for loop from Lower to Upper.
+	for i := Lower; i <= Upper; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Println("FizzBuzz")
+		case i%3 == 0:
+			fmt.Println("Fizz")
+		case i%5 == 0:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
+		}
 }
